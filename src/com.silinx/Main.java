340044eef22940e80a92b3f28ab2d881e9837e73
Swagger-Ranger <liufei32@outlook.com>
2020-05-16 19:41:27 +0800

    测试新key提交

--- src/com.silinx/Main.java
+++ src/com.silinx/Main.java
@@ -1,7 +1,5 @@
 package com.silinx;
 
-import java.util.HashMap;
-
 /**
  * @author Email:liufei32@outlook.com  github:Swagger-Ranger
  * @description
@@ -11,33 +9,41 @@ import java.util.HashMap;
 public class Main {
     public static void main( String[] args ) {
 
-        HashMap<String, Double> map = new HashMap<>();
-        map.put("h1", 0.1);
-        map.put("h2", 0.2);
-        map.put("h3", 0.3);
+//        HashMap<String, Double> map = new HashMap<>();
+//        map.put("h1", 0.1);
+//        map.put("h2", 0.2);
+//        map.put("h3", 0.3);
+//
+//        Thread thread = new Thread(() -> {
+//            String str = "sss";
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println(str);
+//        });
+//
+//        Thread thread1 = new Thread(() -> {
+//            String str = "sss1";
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//            System.out.println(str);
+//        });
+//
+//        thread.start();
+//        thread1.start();
+        int a = 5;
+        int b = a++;
+        int c = ++a;
 
-        Thread thread = new Thread(() -> {
-            String str = "sss";
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(str);
-        });
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
 
-        Thread thread1 = new Thread(() -> {
-            String str = "sss1";
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            System.out.println(str);
-        });
-
-        thread.start();
-        thread1.start();
     }
 }
